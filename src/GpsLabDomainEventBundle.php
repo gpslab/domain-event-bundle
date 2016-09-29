caead5c74a73f2cefa26dfa8bf82a0fa76a2d3f7
Peter Gribanov <wcode404@gmail.com>
2016-09-29 19:19:36 +0300

    configure package

---
+++ src/GpsLabDomainEventBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Bundle\DomainEvent;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GpsLabDomainEventBundle extends Bundle
+{
+}
