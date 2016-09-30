ff43129f5cd5112ee9ddee76d541c97b0baa436d
Peter Gribanov <wcode404@gmail.com>
2016-09-30 16:36:34 +0300

    configure GpsLabDomainEventBundle

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Bundle\DomainEvent\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Config tree builder.
+     *
+     * Example config:
+     *
+     * gpslab_domain_event:
+     *     locator: 'named_event'
+     *     name_resolver: 'event_class'
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        return (new TreeBuilder())
+            ->root('gpslab_domain_event')
+                ->children()
+                    ->scalarNode('locator')
+                        ->cannotBeEmpty()
+                        ->defaultValue('named_event')
+                    ->end()
+                    ->scalarNode('name_resolver')
+                        ->cannotBeEmpty()
+                        ->defaultValue('event_class')
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
---
+++ src/DependencyInjection/GpsLabDomainEventExtension.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Bundle\DomainEvent\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class GpsLabDomainEventExtension extends Extension
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('bus.yml');
+        $loader->load('locator.yml');
+        $loader->load('name_resolver.yml');
+
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $container->setAlias('domain_event.locator', $this->getLocatorRealName($config['locator']));
+        $container->setAlias('domain_event.name_resolver', $this->getNameResolverRealName($config['name_resolver']));
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function getLocatorRealName($name)
+    {
+        if (in_array($name, ['voter', 'named_event'])) {
+            return 'domain_event.locator.'.$name;
+        }
+
+        return $name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function getNameResolverRealName($name)
+    {
+        if (in_array($name, ['event_class', 'event_class_last_part', 'named_event'])) {
+            return 'domain_event.name_resolver.'.$name;
+        }
+
+        return $name;
+    }
+}
