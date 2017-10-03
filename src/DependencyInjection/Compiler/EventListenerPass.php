83de4e8c0e2712b7b1ddd7cba1c52f107af6461a
Peter Gribanov <wcode404@gmail.com>
2017-10-03 12:11:07 +0300

    create methods EventListenerPass::registerListeners() and EventListenerPass::registerSubscribers()

--- src/DependencyInjection/Compiler/EventListenerPass.php
+++ src/DependencyInjection/Compiler/EventListenerPass.php
@@ -11,6 +11,7 @@ namespace GpsLab\Bundle\DomainEvent\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class EventListenerPass implements CompilerPassInterface
 {
@@ -27,17 +28,32 @@ class EventListenerPass implements CompilerPassInterface
         $symfony_locator = $container->findDefinition('domain_event.locator.symfony');
         $container_locator = $container->findDefinition('domain_event.locator.container');
 
-        if ($current_locator !== $symfony_locator && $current_locator !== $container_locator) {
-            return;
+        if ($current_locator === $symfony_locator || $current_locator === $container_locator) {
+            $this->registerListeners($container, $current_locator);
+            $this->registerSubscribers($container, $current_locator);
         }
+    }
 
+    /**
+     * @param ContainerBuilder $container
+     * @param Definition       $current_locator
+     */
+    private function registerListeners(ContainerBuilder $container, Definition $current_locator)
+    {
         foreach ($container->findTaggedServiceIds('domain_event.listener') as $id => $attributes) {
             foreach ($attributes as $attribute) {
                 $method = !empty($attribute['method']) ? $attribute['method'] : '__invoke';
                 $current_locator->addMethodCall('registerService', [$attribute['event'], $id, $method]);
             }
         }
+    }
 
+    /**
+     * @param ContainerBuilder $container
+     * @param Definition       $current_locator
+     */
+    private function registerSubscribers(ContainerBuilder $container, Definition $current_locator)
+    {
         foreach ($container->findTaggedServiceIds('domain_event.subscriber') as $id => $attributes) {
             $subscriber = $container->findDefinition($id);
             $current_locator->addMethodCall('registerSubscriberService', [$id, $subscriber->getClass()]);
