f3abaea04fd7bb012663255faf9420cacd44c056
Peter Gribanov <wcode404@gmail.com>
2019-08-15 12:26:41 +0300

    fix GpsLabDomainEventBundleTest

--- src/GpsLabDomainEventBundle.php
+++ src/GpsLabDomainEventBundle.php
@@ -21,7 +21,6 @@ class GpsLabDomainEventBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
-        parent::build($container);
         $container->addCompilerPass(new EventListenerPass());
     }
 
